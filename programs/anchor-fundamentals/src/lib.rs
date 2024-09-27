a70c919a5d3728d9c39994ed08a9c7a0d44c4551
Hassan <muhammad.hassan@antematter.io>
2024-09-28 03:09:32 +0500

    added a simple integer storage program

--- programs/anchor-fundamentals/src/lib.rs
+++ programs/anchor-fundamentals/src/lib.rs
@@ -7,10 +7,37 @@ pub mod anchor_fundamentals {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        let initial_account = &mut ctx.accounts.initial_account;
+        initial_account.counter = 10;
+        msg!("Initialized!");
+        Ok(())
+    }
+
+    pub fn update_value(ctx: Context<UpdateValue>, value: u64) -> Result<()> {
+        let storage_account = &mut ctx.accounts.storage_account;
+        storage_account.counter = value;
+        msg!("Updated!");
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize <'info>{
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+
+    #[account(init, payer = user, space = 9000)]
+    pub initial_account: Account<'info, Init>
+}
+
+#[derive(Accounts)]
+pub struct UpdateValue <'info>{ 
+    #[account(mut)]
+    pub storage_account: Account<'info, Init>
+}
+
+#[account]
+pub struct Init{
+    pub counter: u64
+}
